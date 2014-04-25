1fc8ffd3477fc84f9832affffc5bdc777e975fd8
Shannon Weyrick <sweyrick@nsone.net>
2014-04-25 18:31:37 -0400

    missing base

---
+++ nsone/rest/service.py
@@ -0,0 +1,21 @@
+#
+# Copyright (c) 2014 NSONE, Inc.
+#
+# License under The MIT License (MIT). See LICENSE in project root.
+#
+
+import requests
+
+class BaseService:
+
+
+
+
+    def __init__(self, config):
+        self._config = config
+        # XXX verify we have a default key
+
+    def _make_request(self):
+        pass
+
+
