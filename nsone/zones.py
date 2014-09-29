bc9be27f029d17d0d5625450e5170c5a0bf39de0
Shannon Weyrick <sweyrick@nsone.net>
2014-09-29 15:15:47 -0400

    documentation

--- nsone/zones.py
+++ nsone/zones.py
@@ -16,7 +16,20 @@ class ZoneException(Exception):
 
 class Zone(object):
 
+    """
+    High level object representing a Zone. In addition to the documented
+    methods, there are magic methods allowing easy creation of records in this
+    zone. Simply can 'add_TYPE' where TYPE is a valid DNS record type, such as
+    add_A(). See examples for more information.
+    """
+
     def __init__(self, config, zone):
+        """
+        Create a new high level Zone object
+
+        :param nsone.config.Config config: config object
+        :param str zone: zone name
+        """
         self._rest = Zones(config)
         self.config = config
         self.zone = zone
@@ -29,9 +42,15 @@ class Zone(object):
         return self.data.get(item, None)
 
     def reload(self, callback=None, errback=None):
+        """
+        Reload zone data from the API.
+        """
         return self.load(reload=True, callback=callback, errback=errback)
 
     def load(self, callback=None, errback=None, reload=False):
+        """
+        Load zone data from the API.
+        """
         if not reload and self.data:
             raise ZoneException('zone already loaded')
 
@@ -46,9 +65,18 @@ class Zone(object):
                                    errback=errback)
 
     def delete(self, callback=None, errback=None):
+        """
+        Delete the zone and ALL records it contains.
+        """
         return self._rest.delete(self.zone, callback=callback, errback=errback)
 
     def update(self, callback=None, errback=None, **kwargs):
+        """
+        Update zone configuration. Pass a list of keywords and their values to
+        update. For the list of keywords available for zone configuration, see
+        :attr:`nsone.rest.zones.Zones.INT_FIELDS` and
+        :attr:`nsone.rest.zones.Zones.PASSTHRU_FIELDS`
+        """
         if not self.data:
             raise ZoneException('zone not loaded')
 
@@ -63,6 +91,12 @@ class Zone(object):
                                  **kwargs)
 
     def create(self, callback=None, errback=None, **kwargs):
+        """
+        Create a new zone. Pass a list of keywords and their values to
+        configure. For the list of keywords available for zone configuration,
+        see :attr:`nsone.rest.zones.Zones.INT_FIELDS` and
+        :attr:`nsone.rest.zones.Zones.PASSTHRU_FIELDS`
+        """
         if self.data:
             raise ZoneException('zone already loaded')
 
@@ -93,6 +127,20 @@ class Zone(object):
     def linkRecord(self, existing_domain, new_domain, rtype,
                    callback=None, errback=None, **kwargs):
 
+        """
+        Create a new linked record in this zone. These records use the
+        configuration (answers, ttl, filters, etc) from an existing record
+        in the NSONE platform.
+
+        :param str existing_domain: FQDN of the target record whose config \
+            should be used. Does not have to be in the same zone.
+        :param str new_domain: Name of the new (linked) record. Zone name is\
+            appended automatically.
+        :param str rtype: DNS record type, which must match the target record.
+        :rtype: nsone.records.Record
+        :return: new Record
+        """
+
         if '.' not in existing_domain:
             existing_domain = existing_domain + '.' + self.zone
 
@@ -105,7 +153,18 @@ class Zone(object):
 
     def cloneRecord(self, existing_domain, new_domain, rtype,
                     zone=None, callback=None, errback=None):
+        """
+        Clone the given record to a new record such that their configs are
+        identical.
 
+        :param str existing_domain: The existing record to clone
+        :param str new_domain: The name of the new cloned record
+        :param str rtype: DNS record type
+        :param str zone: Optional zone name, if the new record should exist in\
+            a different zone than the original record.
+        :rtype: nsone.records.Record
+        :return: new Record
+        """
         if zone is None:
             zone = self.zone
 
@@ -138,15 +197,34 @@ class Zone(object):
                                errback=errback)
 
     def loadRecord(self, domain, rtype, callback=None, errback=None):
-        """ :type result: nsone.records.Record """
+        """
+        Load a high level Record object from a domain within this Zone.
+
+        :param str domain: The name of the record to load
+        :param str rtype: The DNS record type
+        :rtype: nsone.records.Record
+        :return: new Record
+        """
         rec = Record(self, domain, rtype)
         return rec.load(callback=callback, errback=errback)
 
     def qps(self, callback=None, errback=None):
+        """
+        Return the current QPS for this zone
+
+        :rtype: dict
+        :return: QPS information
+        """
         stats = Stats(self.config)
         return stats.qps(zone=self.zone, callback=callback, errback=errback)
 
     def usage(self, callback=None, errback=None, **kwargs):
+        """
+        Return the current usage information for this zone
+
+        :rtype: dict
+        :return: usage information
+        """
         stats = Stats(self.config)
         return stats.usage(zone=self.zone, callback=callback, errback=errback,
                            **kwargs)
