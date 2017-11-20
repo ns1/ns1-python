f848eb093659503db29c96222844c78984a76be7
Steve Coursen <scoursen@ns1.com>
2017-11-20 10:00:35 -0500

    Add support for calling zone_search endpoint

--- ns1/rest/zones.py
+++ ns1/rest/zones.py
@@ -60,3 +60,14 @@ class Zones(resource.BaseResource):
         return self._make_request('GET', '%s/%s' % (self.ROOT, zone),
                                   callback=callback,
                                   errback=errback)
+
+    def search(self, zone, q=None, has_geo=False, callback=None, errback=None):
+        params = {}
+        if q is not None:
+            params['q'] = q
+        if has_geo:
+            params['has_geo'] = has_geo
+        return self._make_request('GET', '%s/%s/search' % (self.ROOT, zone),
+                                  params=params,
+                                  callback=callback,
+                                  errback=errback)
