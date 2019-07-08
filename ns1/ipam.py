be8433d0d352282e877bdf2aa4140858957e9cb4
Christian Grabowski <cgrabowski@ns1.com>
2019-07-08 14:30:04 -0400

    use id in retrieve call

--- ns1/ipam.py
+++ ns1/ipam.py
@@ -466,7 +466,7 @@ class Scopegroup(object):
 
 class Reservation(object):
 
-    def __init__(self, config, scopegroup_id, address_id, options=None, mac=None):
+    def __init__(self, config, scopegroup_id, address_id, reservation_id=None, options=None, mac=None):
         """
         Create a new high level Reservation object
 
@@ -478,6 +478,7 @@ class Reservation(object):
         """
         self._rest = Reservations(config)
         self.config = config
+        self.id = reservation_id
         self.scopegroup_id = scopegroup_id
         self.address_id = address_id
         self.mac = mac
@@ -519,10 +520,10 @@ class Reservation(object):
             else:
                 return self
 
-        if self.scopegroup_id is None or self.address_id is None:
-            raise ReservationException('Must specify a scopegroup_id and an address_id')
+        if self.id is None:
+            raise ReservationException('Must specify a scope_id')
 
-        return self._rest.retrieve(self.scopegroup_id, self.address_id, callback=success,
+        return self._rest.retrieve(self.id, callback=success,
                                    errback=errback)
 
     def delete(self, callback=None, errback=None):
@@ -554,7 +555,7 @@ class Reservation(object):
 
 class Scope(object):
 
-    def __init__(self, config, scopegroup_id, address_id, options=None):
+    def __init__(self, config, scopegroup_id, address_id, scope_id=None, options=None):
         """
         Create a new high level Scope object
 
@@ -567,6 +568,7 @@ class Scope(object):
         self.config = config
         self.scopegroup_id = scopegroup_id
         self.address_id = address_id
+        self.id = scope_id
 
         if options is None:
             options = DHCPOptions('dhcpv4', {})
@@ -606,10 +608,10 @@ class Scope(object):
             else:
                 return self
 
-        if self.scopegroup_id is None or self.address_id is None:
-            raise ReservationException('Must specify a scopegroup_id and an address_id')
+        if self.id is None:
+            raise ScopeException('Must specify a scope_id')
 
-        return self._rest.retrieve(self.scopegroup_id, self.address_id, callback=success,
+        return self._rest.retrieve(self.id, callback=success,
                                    errback=errback)
 
     def delete(self, callback=None, errback=None):
