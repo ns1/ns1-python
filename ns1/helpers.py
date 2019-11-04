8e971e259be9d4c217aa8ee387e90d24c7fbf049
rupa <rdeadwyler@ns1.com>
2019-11-04 13:36:25 -0500

    INT-78: Add a helper class for concurrency (#40)
    SingletonMixin helps make your class a thread safe singleton, via
    double-checked locking.
    
    Useful for scenarios where we have multiple threads or processes
    going and e.g. need to deal with client rate-limits correctly.

---
+++ ns1/helpers.py
@@ -0,0 +1,14 @@
+from threading import Lock
+
+
+class SingletonMixin(object):
+    """double-locked thread safe singleton"""
+    _instance = None
+    _lock = Lock()
+
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            with cls._lock:
+                if cls._instance is None:
+                    cls._instance = object.__new__(cls, *args, **kwargs)
+        return cls._instance
