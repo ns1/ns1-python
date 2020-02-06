1ce9cb885a7b61d57c783a24fc2563cab4989940
rupa deadwyler <rdeadwyler@ns1.com>
2020-02-06 14:45:54 -0500

    Add endpoints to "high level interface"
    Adds methods to NS1 class
      * monitoring_jobtypes: returns ns1.rest.monitoring.JobTypes
      * monitoring_regions: returns ns1.rest.monitoring.Regions
    
    Also started some tests for ns1/__init__

---
+++ tests/unit/test_init.py
@@ -0,0 +1,66 @@
+import pytest
+
+from ns1 import NS1
+from ns1.rest.account import Plan
+from ns1.rest.apikey import APIKey
+from ns1.rest.data import Feed, Source
+from ns1.rest.ipam import (
+    Addresses,
+    Networks,
+    Reservations,
+    Scopegroups,
+    Scopes,
+    Optiondefs,
+)
+from ns1.rest.monitoring import JobTypes, Monitors, NotifyLists, Regions
+from ns1.rest.records import Records
+from ns1.rest.stats import Stats
+from ns1.rest.team import Team
+from ns1.rest.user import User
+from ns1.rest.zones import Zones
+
+
+@pytest.fixture
+def ns1_config(config):
+    config.loadFromDict(
+        {
+            "endpoint": "api.nsone.net",
+            "default_key": "test1",
+            "keys": {
+                "test1": {
+                    "key": "key-1",
+                    "desc": "test key number 1",
+                    "writeLock": True,
+                }
+            },
+        }
+    )
+
+    return config
+
+
+@pytest.mark.parametrize("method, want", [
+    ("zones", Zones),
+    ("records", Records),
+    ("addresses", Addresses),
+    ("networks", Networks),
+    ("scope_groups", Scopegroups),
+    ("reservations", Reservations),
+    ("scopes", Scopes),
+    ("optiondefs", Optiondefs),
+    ("stats", Stats),
+    ("datasource", Source),
+    ("datafeed", Feed),
+    ("monitors", Monitors),
+    ("notifylists", NotifyLists),
+    ("monitoring_jobtypes", JobTypes),
+    ("monitoring_regions", Regions),
+    ("plan", Plan),
+    ("team", Team),
+    ("user", User),
+    ("apikey", APIKey),
+])
+def test_rest_interface(ns1_config, method, want):
+    client = NS1(config=ns1_config)
+    got = getattr(client, method)()
+    assert isinstance(got, want)
